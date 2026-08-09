c3b75d2d277277f351d8b0f65a83a93d4a7bc92f
Xingyu Wang <xingyu.wang@gmail.com>
2026-08-09 21:57:16 +0800

    chore: convert Xcode project to macOS app, link local DiskReservoirCore, add M3 plan

---
+++ PoolProblem/PoolProblem/PoolProblemApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoolProblemApp.swift
+//  PoolProblem
+//
+//  Created by xingyu wang on 2026/8/9.
+//
+
+import SwiftUI
+
+@main
+struct PoolProblemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
